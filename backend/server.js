6bb807e0235bcabc2d9911625a508d6b6c8f083f
Anant Chitranshi <anantchitranshi001@gmail.com>
2026-05-14 11:33:17 +0530

    Updated PORT to 5000 for Render

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.get("/health", (req, res) => {
 });
 
 // Port and DB configuration
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
